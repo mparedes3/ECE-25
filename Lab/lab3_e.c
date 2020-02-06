31ebfdaf55668f21af1622e1db9bf9ba92ff8dfb
Marco Paredes <marco.paredes2001@gmail.com>
2020-02-05 19:43:46 -0800

    Add files via upload

---
+++ Lab/lab3_e.c
@@ -0,0 +1,17 @@
+// CL2947MP_
+
+module lab3_e(input CL2947MP_x, input CL2947MP_y, input CL2947MP_z, input CL2947MP_w, output CL2947MP_out);
+  wire CL2947MP_xnot, CL2947MP_ynot, CL2947MP_znot, CL2947MP_wnot;
+  wire CL2947MP_1, CL2947MP_2, CL2947MP_3, CL2947MP_4;
+
+  not not(CL2947MP_xnot, CL2947MP_x);
+  not not(CL2947MP_znot, CL2947MP_z);
+
+  and and(CL2947MP_1, CL2947MP_w, CL2947MP_x);
+  and and(CL2947MP_2, CL2947MP_w, CL2947MP_y);
+  and and(CL2947MP_3, CL2947MP_xnot, CL2947MP_znot);
+  and and(CL2947MP_4, CL2947MP_y, CL2947MP_znot);
+
+  or or(CL2947MP_out, CL2947MP_1, CL2947MP_2, CL2947MP_3, CL2947MP_4);
+
+endmodule
