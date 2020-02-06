6e407952a31eb5ad7256688f065d44e209c54d36
Marco Paredes <marco.paredes2001@gmail.com>
2020-02-05 19:46:35 -0800

    Add files via upload

--- Lab 3/Prelab/prelab3_b.c
+++ Lab 3/Prelab/prelab3_b.c
@@ -4,17 +4,17 @@ module prelab3_b(input CL2947MP_x, input CL2947MP_y, input CL2947MP_z, input CL2
   wire CL2947MP_xnot, CL2947MP_ynot, CL2947MP_znot, CL2947MP_wnot;
   wire CL2947MP_1, CL2947MP_2, CL2947MP_3, CL2947MP_4, CL2947MP_5;
 
-  not not(CL2947MP_xnot, CL2947MP_x);
-  not not(CL2947MP_ynot, CL2947MP_y);
-  not not(CL2947MP_znot, CL2947MP_z);
-  not not(CL2947MP_wnot, CL2947MP_w);
+  not not1(CL2947MP_xnot, CL2947MP_x);
+  not not2(CL2947MP_ynot, CL2947MP_y);
+  not not3(CL2947MP_znot, CL2947MP_z);
+  not not4(CL2947MP_wnot, CL2947MP_w);
 
-  and and(CL2947MP_1, CL2947MP_xnot, CL2947MP_znot);
-  and and(CL2947MP_2, CL2947MP_xnot, CL2947MP_wnot);
-  and and(CL2947MP_3, CL2947MP_y, CL2947MP_z, CL2947MP_w);
-  and and(CL2947MP_4, CL2947MP_y, CL2947MP_z, CL2947MP_wnot);
-  and and(CL2947MP_5, CL2947MP_ynot, CL2947MP_xnot, CL2947MP_wnot);
+  and and1(CL2947MP_1, CL2947MP_xnot, CL2947MP_znot);
+  and and2(CL2947MP_2, CL2947MP_xnot, CL2947MP_wnot);
+  and and3(CL2947MP_3, CL2947MP_y, CL2947MP_z, CL2947MP_w);
+  and and4(CL2947MP_4, CL2947MP_y, CL2947MP_z, CL2947MP_wnot);
+  and and5(CL2947MP_5, CL2947MP_ynot, CL2947MP_xnot, CL2947MP_wnot);
 
-  or or(CL2947MP_out, CL2947MP_1, CL2947MP_2, CL2947MP_3, CL2947MP_4, CL2947MP_5);
+  or or1(CL2947MP_out, CL2947MP_1, CL2947MP_2, CL2947MP_3, CL2947MP_4, CL2947MP_5);
 
 endmodule
